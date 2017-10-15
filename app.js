7f4f610449686cd9fe1190c9d63aa6a495621ddb
llabake <lemboyelabake@gmail.com>
2017-10-15 15:21:38 +0100

    add eslintrc babelrc and hound.yml file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
-import routes from './server/routes';
-import db from './server/models';
+// import routes from './server/routes';
+// import db from './server/models';
  
 // Set up the express app
 const app = express();
@@ -10,14 +10,14 @@ const app = express();
 // Log requests to the console.
 app.use(logger('dev'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Parse incoming requests data 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Call all routes with app instance
-routes(app);
+// routes(app);
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
