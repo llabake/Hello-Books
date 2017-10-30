0ac69f19c8b08cc197c5b020ee447096eeff3e16
llabake <lemboyelabake@gmail.com>
2017-10-30 14:24:37 +0100

    modify user exist middleware to check user model not book model

--- server/middlewares/userExistsMiddleware.js
+++ server/middlewares/userExistsMiddleware.js
@@ -3,7 +3,7 @@ import { dummyData } from '../helpers/modelHelpers';
 
 const userExists = (req, res, next) => {
     const userId = req.params.userId;
-    if (dummyData.books.hasOwnProperty(userId)) {
+    if (dummyData.users.hasOwnProperty(userId)) {
         next();
     } else {
        return res.status(404).json({message: "User not found."})
