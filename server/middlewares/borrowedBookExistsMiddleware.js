61e01f2f957d764a9f2bd49ba94c2f74b868e535
llabake <lemboyelabake@gmail.com>
2017-10-31 12:44:45 +0100

    fix borrowed book middleware problem

--- server/middlewares/borrowedBookExistsMiddleware.js
+++ server/middlewares/borrowedBookExistsMiddleware.js
@@ -2,14 +2,15 @@ import BorrowedBook from '../models/borrowedBook';
 
 
 const borrowedBookExists = (req, res, next) => {
-    const userId = parseInt(req.params.userId, 10);
-    const bookId = parseInt(req.params.bookId, 10);
-    
-    if (!BorrowedBook.existsByUserIdAndBookId(userId, bookId)) {
-        return res.status(400).json({message: "Request to borrow book hasn't been made"})        
-    } else {
-        next()
-    }
-}
+  const userId = parseInt(req.params.userId, 10);
+  const bookId = parseInt(req.params.bookId, 10);
+
+  if (!BorrowedBook.existsByUserIdAndBookId(userId, bookId)) {
+    return res.status(400).json({
+      message: "Request to borrow book hasn't been made"
+    });
+  }
+  next();
+};
 
 export default borrowedBookExists;
