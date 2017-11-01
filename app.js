dc7e6c578cbb35a284b9ad925d765640775ef3b7
llabake <lemboyelabake@gmail.com>
2017-11-01 16:33:26 +0100

    fix port listening in test environment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import routes from './server/routes';
- 
+
 // Set up the express app
 const app = express();
 
@@ -11,20 +11,26 @@ app.use(logger('dev'));
 
 const port = process.env.PORT || 5000;
 
-// Parse incoming requests data 
+// Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Call all routes with app instance
+// Call all routes with app instance
 routes(app);
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
+/*
+  Setup a default catch-all route that sends
+  back a welcome message in JSON format.
+*/
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to Hello Books.',
 }));
 
-app.listen(port, () => {
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-});
+  });
+}
+
 
 export default app;
