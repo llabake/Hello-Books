51cc31200feabfb914d4cff240b556fbc0a528f6
llabake <lemboyelabake@gmail.com>
2017-11-11 10:55:51 +0100

    add functionality to verify user token

---
+++ server/middlewares/authenticationMiddleware.js
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken';
+
+const secret = 'mysecret';
+
+/**
+ * Class Definition for the Authentication Object using JWT
+ *
+ * @export
+ * @class Authentication
+ */
+export default class Authentication {
+/**
+ * @description Middleware to verify the supplied token
+ *
+ * @static
+ * @param {Object} req request object
+ * @param {Object} res response object
+ * @param {Function} next callback function
+ * @returns {Object} response containing user's access status
+ *
+ * @memberof Authentication
+ */
+  static authMiddleware(req, res, next) {
+    const token = req.headers.authorization;
+    if (!(token)) {
+      return res.status(401).json({
+        error: 'Missing token.Expects token in header with key as Authorization'
+      });
+    }
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        if (err.message === 'jwt expired') {
+          res.status(401).json({
+            error: 'Access token has expired. You are required to login again'
+          });
+        } else {
+          res.status(401).json({
+            error: 'Authentication failed. Invalid access token'
+          });
+        }
+      }
+      req.user = decoded.data;
+      next();
+    });
+  }
+}
