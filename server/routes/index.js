01ed9e9e244c7e0621139b97987ddde331b325c4
llabake <lemboyelabake@gmail.com>
2017-11-11 07:54:56 +0100

    add user route to index route file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import IndexRoute from './indexRoute';
 import bookRoute from './bookRoute';
+import userRoute from './userRoute';
 
 const routes = (app) => {
   IndexRoute(app);
   bookRoute(app);
+  userRoute(app);
 };
 
 export default routes;
