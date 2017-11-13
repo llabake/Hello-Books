7c6a12273eab1acb6faf7c581a1a1c58387a368a
llabake <lemboyelabake@gmail.com>
2017-11-13 12:37:48 +0100

    add function to generate token

--- server/helpers/utils.js
+++ server/helpers/utils.js
@@ -1,3 +1,7 @@
+import jwt from 'jsonwebtoken';
+
+const secret = 'mySecret';
+
 export const isEmail = (email) => {
   const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
   return re.test(email);
@@ -6,4 +10,26 @@ export const isEmail = (email) => {
 export const isAlphanumeric = (str) => {
   const re = /^([0-9]|[a-z])+([0-9a-z]+)$/i;
   return re.test(str);
+};
+
+/**
+   *
+   * @description: generate a login token
+   * @param {Object} user payload to generate token
+   * @returns {String} the gnerated token
+   */
+export const generateToken = (user) => {
+  const token = jwt.sign(
+    {
+      user: {
+        id: user.id,
+        role: user.role,
+        email: user.email,
+        username: user.username
+      }
+    },
+    secret,
+    { expiresIn: '24h' }
+  );
+  return token;
 };
