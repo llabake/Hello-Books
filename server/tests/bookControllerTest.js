7190c77f0434764484e53991c8f59ef2d1137847
llabake <lemboyelabake@gmail.com>
2017-11-16 22:44:09 +0100

    fix xit problem in book controller test

--- server/tests/bookControllerTest.js
+++ server/tests/bookControllerTest.js
@@ -402,7 +402,7 @@ describe('Controller Functions', () => {
         });
     });
     // pending accept
-    xxit('should return borrowedbook with status accepted' +
+    xit('should return borrowedbook with status accepted' +
     ' for an accepted request', (done) => {
       const bookData = {
         title: 'so long a letter',
