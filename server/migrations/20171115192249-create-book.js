7e35aa6a0e80fde2cb8b0fe952e00b4526fc6bef
llabake <lemboyelabake@gmail.com>
2017-11-16 17:52:12 +0100

    add book model and migration files

---
+++ server/migrations/20171115192249-create-book.js
@@ -0,0 +1,55 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Books', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    title: {
+      type: Sequelize.STRING
+    },
+    author: {
+      type: Sequelize.STRING
+    },
+    publishedYear: {
+      type: Sequelize.STRING
+    },
+    isbn: {
+      type: Sequelize.STRING,
+      unique: true
+    },
+    quantity: {
+      type: Sequelize.INTEGER
+    },
+    description: {
+      type: Sequelize.TEXT
+    },
+    readingList: {
+      type: Sequelize.STRING
+    },
+    image: {
+      type: Sequelize.TEXT
+    },
+    upVotes: {
+      type: Sequelize.INTEGER
+    },
+    downVotes: {
+      type: Sequelize.INTEGER
+    },
+    borrowCount: {
+      type: Sequelize.INTEGER
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Books')
+};
