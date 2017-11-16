d8b9e1b730ee176521dd5a4a8cd44b73c5d4f0d9
llabake <lemboyelabake@gmail.com>
2017-11-16 18:00:31 +0100

    remove book association from user model

--- server/models/user.js
+++ server/models/user.js
@@ -100,9 +100,9 @@ export default (sequelize, DataTypes) => {
     },
   });
   User.associate = (models) => {
-    User.hasMany(models.Book, {
-      foreignKey: 'userId'
-    });
+    // User.hasMany(models.Book, {
+    //   foreignKey: 'userId'
+    // });
     // User.hasMany(models.Review, {
     //   foreignKey: 'userId'
     // });
