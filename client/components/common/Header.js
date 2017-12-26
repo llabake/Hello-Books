16ae16cc0a9734ffb0f4dd5fb9569a49a8dcf77a
llabake <lemboyelabake@gmail.com>
2017-12-26 23:11:25 +0100

    use reusuable component in app component

--- client/components/common/Header.js
+++ client/components/common/Header.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
+import SearchBar from './SearchBar';
 
 const Header = () => {
-    return (
-        <header>
-            <div className="navbar-fixed">
-                <nav>
-                    <div className="nav-wrapper">
+  return (
+    <header>
+      <div className="navbar-fixed">
+        <nav>
+          <div className="nav-wrapper">
             <NavLink to="/" 
             className="brand-logo left adjust">HelloBooks
             </NavLink>
-                        <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-                        <ul className="right hide-on-med-and-down">
-                            <li>
-                                <form>
-                                    <div className="input-field col s6 s12 ">
-                                        <input id="search" type="search"  placeholder="Title, author, or ISBN" required/>
-                                        <label className="label-icon icon-sit" htmlFor="search"><i className="material-icons">search</i></label>
-                                        <i className="material-icons">close</i>
-                                    </div>
-                                </form>
-                            </li>
+            <a href="#" data-activates="mobile-demo" className="button-collapse">
+              <i className="material-icons">menu</i>
+            </a>
+            <ul className="right hide-on-med-and-down">
+              <li>
+                <SearchBar />
+              </li>
               <li>
                 <NavLink to="/signin">Sign In
                 </NavLink>
@@ -30,17 +27,11 @@ const Header = () => {
                 <NavLink to="/signup">Sign Up
                 </NavLink>
               </li>
-                        </ul>
-                        <ul className="side-nav" id="mobile-demo">
-                            <li>
-                                <form>
-                                    <div className="input-field col s6 s12 ">
-                                        <input id="search" type="search"  placeholder="Title, author, or ISBN" required/>
-                                        <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
-                                        <i className="material-icons">close</i>
-                                    </div>
-                                </form>
-                            </li>
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+              <li>
+                <SearchBar />
+              </li>
               <li>
                 <Link to="#popular-books">Popular Books
                 </Link></li> 
@@ -50,12 +41,12 @@ const Header = () => {
               <li>
                 <NavLink to="/signup">Sign Up
                 </NavLink></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </header>
-    );
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
