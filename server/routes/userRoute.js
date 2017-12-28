98562694d2d7532d0a845eace3765b7ccda6aa4b
llabake <lemboyelabake@gmail.com>
2017-12-28 08:11:53 +0100

    add functionality to check if username or email is already taken and the corresponding route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -74,6 +74,32 @@ const userRoute = (app) => {
     '/api/v1/users/signout',
     Authentication.authMiddleware, UserController.signOut
   );
+
+  /**
+  * @swagger
+  * /api/v1/users/signup:
+  *   get:
+  *     tags:
+  *       - User Functionality
+  *     description: Checks if user exists
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: user
+  *         description: User object
+  *         in: body
+  *         required: true
+  *         schema:
+  *           $ref: '#/definitions/User'
+  *     responses:
+  *       200:
+  *         description: Successfully found a user
+  *       404:
+  *         description: User not found
+  */
+  app.get(
+    '/api/v1/users/signup', UserController.checkUserExist
+  )
 };
 
 export default userRoute;
