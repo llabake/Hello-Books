13eef6204690f3fa1fb462ffcf82a9c525138480
llabake <lemboyelabake@gmail.com>
2017-12-30 17:55:51 +0100

    add store to the app entry point

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
 
 // main app
 import App from '../components/App';
+import store from '../store';
 
-render(<App />, document.getElementById('app'))
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('app'))
