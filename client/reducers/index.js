4d7008c9d4a295bf9d79bd06f27467e7c50bff1e
llabake <lemboyelabake@gmail.com>
2018-01-02 15:20:34 +0100

    change sign up reducer to user reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import signUpReducer from './signUpReducer'
+import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
-  signUpReducer
+  userReducer
 });
 
 export default  rootReducer;
