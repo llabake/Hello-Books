612cb80dd5e5965e0e4c5b89b4d531b836fc7ded
llabake <lemboyelabake@gmail.com>
2018-01-05 17:48:29 +0100

    add add book component

--- client/components/App.js
+++ client/components/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
 import SignUpForm from './SignUpForm';
 import SignInForm from './SignInForm';
-import AllBooks from './main/AllBooks';
+import AllBooks from './main/AllBooks'
+import AddBook from './main/AddBook';
 
 import '../styles/style.scss';
 /**
@@ -31,6 +32,7 @@ export default class App extends Component {
           <Route exact path='/signup' component={SignUpForm} />
           <Route exact path='/signin' component={SignInForm} />
           <Route exact path='/allbooks' component={AllBooks} />
+          <Route exact path='/addbook' component={AddBook} />
         </Switch>
         </div>
       </Router>
