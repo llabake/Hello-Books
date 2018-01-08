04dd4f8604a3cc194b579367a76cb648c88eeb19
llabake <lemboyelabake@gmail.com>
2018-01-08 15:54:41 +0100

    include book to add revie response and add get all book revie functionality

--- server/controllers/v1/reviewController.js
+++ server/controllers/v1/reviewController.js
@@ -1,7 +1,7 @@
 import models from '../../models';
 import InputValidator from '../../helpers/inputValidator';
 
-const { Review } = models;
+const { Review, Book, User } = models;
 /**
  *
  *
@@ -23,21 +23,63 @@ export default class ReviewController {
     if (!isValid) {
       res.status(400).json({ errors });
     } else {
-      Review.create({
-        content: req.body.content,
-        bookId: req.params.bookId,
-        userId: req.user.id
+      Book.findOne({
+        where: {
+          id: req.params.bookId
+        },
+        // exclude: [{
+        //   attributes: ['createdAt', 'updatedAt']
+        // }],
+        include: [{
+          model: Review,
+          as: 'reviews',
+          attributes: ['id', 'content', 'createdAt', 'caption'],
+          include: [{
+            model: User,
+            as: 'user',
+            attributes: ['username', 'id'],
+          }],
+        }]
       })
-        .then((review) => {
-          res.status(201).json({
-            message: 'Review has been posted',
-            review
+      .then((book) => {
+        if (book) {
+          Review.create({
+            content: req.body.content,
+            caption: req.body.caption,
+            bookId: req.params.bookId,
+            userId: req.user.id
+          })
+            .then(() => {
+              book.reload()
+              .then((reviewedBook) => {
+                res.status(201).json({
+                  message: 'Review has been posted',
+                  reviewedBook
+                });
+              })
+              .catch((error) => {
+                res.status(500).json({
+                  message: error.message
+                })
+              })
+
+            })
+            .catch(error => res.status(500).json({
+              message: 'error sending your request',
+              error
+            }));
+        } else {
+          res.status(404).json({
+            message: `Book with id: ${req.params.bookId} not found`,
           });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: error.message
         })
-        .catch(error => res.status(500).json({
-          message: 'error sending your request',
-          error
-        }));
+      })
+
     }
   }
   /**
@@ -77,6 +119,45 @@ export default class ReviewController {
         error,
         message: 'error sending your request'
       }));
+  }
+
+  /**
+   * @returns {Array} Array of review on a book
+   * 
+   * @static
+   * @param {any} req 
+   * @param {any} res 
+   * @memberof ReviewController
+   */
+  static getAllBookReview(req, res) {
+    Review.findAll({
+      where: {
+        bookId: req.params.bookId
+      },
+      include: [{
+        model: User,
+        as: 'user',
+        attributes: ['username', 'id',],
+      }],
+    })
+    .then((reviews) => {
+      if (reviews.length === 0) {
+        return res.status(200).json({
+          reviews,
+          message: 'Be the first to post a review...'
+        })
+      }
+      return res.status(200).json({
+        message: 'Reviews retrieved successfully',
+        reviews
+      })
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'error sending your request',
+        error: error.message
+      });
+    })
   }
 }
 
