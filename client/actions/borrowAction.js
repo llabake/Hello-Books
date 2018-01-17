cbca712d514aa59e80f67583f4190a85323874f5
llabake <lemboyelabake@gmail.com>
2018-01-17 22:23:01 +0100

    add actions to borro and return book

--- client/actions/borrowAction.js
+++ client/actions/borrowAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toastMessage from '..//helpers/toastMessage';
 import { hostUrl } from '../helpers/utils';
-import bluebird from 'bluebird';
+import bluebird, { Promise } from 'bluebird';
 import axiosDefaultOptions from '../helpers/axiosDefaultOptions';
 import { 
   FETCH_BOOK_SUCCESS,
@@ -14,7 +14,16 @@ import {
   PENDING_RETURN_REQUEST_ERROR,
   DELETE_BOOK,
   DELETE_BOOK_SUCCESS,
-  DELETE_BOOK_ERROR
+  DELETE_BOOK_ERROR,
+  UPDATE,
+  UPDATE_SUCCESS,
+  UPDATE_ERROR,
+  ACCEPT_BOOK_BORROW,
+  ACCEPT_BOOK_BORROW_SUCCESS,
+  ACCEPT_BOOK_BORROW_ERROR,
+  ACCEPT_BOOK_RETURN,
+  ACCEPT_BOOK_RETURN_SUCCESS,
+  ACCEPT_BOOK_RETURN_ERROR,
 } from './actionTypes';
 
 const fetchBookSuccess = (books) => {
@@ -98,10 +107,8 @@ export const pendingAcceptReturnRequest = () => dispatch => {
   dispatch(pendingAcceptReturn());
   return axios.get(`${hostUrl}/api/v1/borrowedbooks/?returnStatus=pending`, axiosDefaultOptions)
   .then((response) => {
-    console.log(response.data)
-    
-    dispatch(pendingAcceptReturnSuccess(response.data.borrowedBooks))
-    // toastMessage(response.data.message, 'success')
+    dispatch(pendingAcceptReturnSuccess(response.data))
+    toastMessage(response.data.message, 'success')
   })
   .catch((error) => {
     dispatch(pendingAcceptReturnError(error))
@@ -141,4 +148,110 @@ export const deleteBookAction = (bookId) => dispatch => {
       toastMessage(error.response.data.message, 'failure')
     })
 
+}
+
+
+const update = () => {
+  return {
+    type: UPDATE
+  }
+}
+
+const updateSuccess = (book) => {
+  return {
+    type: UPDATE_SUCCESS,
+    book
+  }
+}
+
+const updateError = (error) => {
+  return {
+    type: UPDATE_ERROR,
+    error
+  }
+}
+
+
+export const updateBook = (bookId, bookData) => dispatch => {
+  dispatch(update());
+  return new Promise((resolve, reject) => {
+    return axios.put(`${hostUrl}/api/v1/books/${bookId}`, bookData, axiosDefaultOptions)
+    .then((response) => {
+      dispatch(updateSuccess(response.data.book))
+      toastMessage(response.data.message, 'success')
+      resolve(response)
+    })
+    .catch((error) => {
+      dispatch(updateError(error));
+      toastMessage(error.response.data.message, 'failure')
+      reject(error)
+    })
+  })
+}
+
+const acceptBookBorrow = () => {
+  return {
+    type: ACCEPT_BOOK_BORROW
+  }
+}
+
+const acceptBookBorrowSuccess = (borrowedBook,) => {
+  return {
+    type: ACCEPT_BOOK_BORROW_SUCCESS,
+    borrowedBook,
+    
+  }
+}
+
+const acceptBookBorrowError = (error) => {
+  return {
+    type: ACCEPT_BOOK_BORROW_ERROR,
+    error
+  }
+}
+
+export const acceptBookBorrowRequest = (bookId, userId) => dispatch => {
+  dispatch(acceptBookBorrow());
+  return axios.put(`${hostUrl}/api/v1/admin/user/${userId}/borrow/${bookId}`, {},  axiosDefaultOptions)
+  .then((response) => {
+    dispatch(acceptBookBorrowSuccess(response.data.borrowedBook))
+    toastMessage(response.data.message, 'success')
+  })
+  .catch((error) => {
+    dispatch(acceptBookBorrowError(error))
+    toastMessage(error.response.data.message, 'failure')
+  })
+}
+
+const acceptBookReturn = () => {
+  return {
+    type: ACCEPT_BOOK_RETURN
+  }
+}
+
+const acceptBookReturnSuccess = (returnedBook) => {
+  return {
+    type: ACCEPT_BOOK_RETURN_SUCCESS,
+    returnedBook
+  }
+}
+
+const acceptBookReturnError = (error) => {
+  return {
+    type: ACCEPT_BOOK_RETURN_ERROR,
+    error
+  }
+}
+
+export const acceptBookReturnRequest = (bookId, userId) => dispatch => {
+  dispatch(acceptBookReturn());
+  return axios.put(`${hostUrl}/api/v1/admin/user/${userId}/return/${bookId}`, {},  axiosDefaultOptions)
+  .then((response) => {
+    dispatch(acceptBookReturnSuccess(response.data.borrowedBook))
+    toastMessage(response.data.message, 'success')
+  })
+  .catch((error) => {
+    dispatch(acceptBookReturnError(error))
+    toastMessage(error.response.data.message, 'failure')
+  })
 }
