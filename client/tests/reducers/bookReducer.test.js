05a1859a6c9e1cfc6868c19a3f1129aa64fae122
llabake <lemboyelabake@gmail.com>
2018-02-28 14:25:28 +0100

    update book reducer tests

--- client/tests/reducers/bookReducer.test.js
+++ client/tests/reducers/bookReducer.test.js
@@ -2,7 +2,10 @@ import expect from 'expect';
 import reducer from '../../reducers/bookReducer';
 import * as types from '../../actions/actionTypes';
 import initialState from '../../reducers/initialState'
-import {books, error, book2, book3, addfavbook, upvotedBook, downvotedBook } from '../reducers/mocks/booksData';
+import {books, error, book2, book3, addfavbook,
+  upvotedBook, downvotedBook, book3AfterReviewDeletion,
+  reviewToEdit, book3AfterReviewUpdate, popularBooks,
+  topFavoriteBooks, editedReview } from '../reducers/mocks/booksData';
 
 describe('Book reducer', () => {
   it('should return the initial state', () => {
@@ -22,11 +25,11 @@ describe('Book reducer', () => {
     expect(reducer({}, fetchAllBooksSuccess)).toEqual({books, loading: false})
   });
   it('should handle FETCH_BOOK_ERROR', () => {
-    const fetchAllBooksSuccess = {
+    const fetchAllBooksError = {
       type: types.FETCH_BOOK_ERROR,
       error
     };
-    expect(reducer({}, fetchAllBooksSuccess)).toEqual({error, loading: false})
+    expect(reducer({}, fetchAllBooksError)).toEqual({error, loading: false})
   });
   it('should handle FETCH SINGLE BOOK', () => {
     const fetchSingleBook = {
@@ -90,6 +93,146 @@ describe('Book reducer', () => {
     };
     expect(reducer({}, downvoteBookError)).toEqual({ error })
   });
-
+  it('should handle SHOW_REVIEW_TEXT_AREA', () => {
+    const showTextArea = {
+      type: types.SHOW_REVIEW_TEXT_AREA
+    }
+    expect(reducer({}, showTextArea)).toEqual({ loadAllReview: false, loadTextArea: true })
+  });
+  it('should handle SHOW_ALL_REVIEWS', () => {
+    const showTextArea = {
+      type: types.SHOW_ALL_REVIEWS
+    }
+    expect(reducer({}, showTextArea)).toEqual({ loadAllReview: true, loadTextArea: false })
+  });
+  it('should handle REVIEW_SUCCESS', () => {
+    const reviewBookSuccess = {
+      type: types.REVIEW_SUCCESS,
+      reviewedBook: book2
+    };
+    expect(reducer({}, reviewBookSuccess)).toEqual({ loadTextArea: false, book:book2 })
+  });
+  it('should handle REVIEW_ERROR', () => {
+    const reviewBookError = {
+      type: types.REVIEW_ERROR,
+      error
+    };
+    expect(reducer({}, reviewBookError)).toEqual({ error })
+  });
+  it('should handle BORROW_SUCCESS', () => {
+    const borrowBookSuccess = {
+      type: types.BORROW_SUCCESS,
+      borrowedBook: book2
+    };
+    expect(reducer({}, borrowBookSuccess)).toEqual({ borrowedBook:book2 })
+  });
+  it('should handle BORROW_ERROR', () => {
+    const borrowBookError = {
+      type: types.BORROW_ERROR,
+      error
+    };
+    expect(reducer({}, borrowBookError)).toEqual({ error })
+  });
+  it('should handle SEARCH_ALL_BOOKS_SUCCESS', () => {
+    const searchAllBooksSuccess = {
+      type: types.SEARCH_ALL_BOOKS_SUCCESS,
+      books
+    };
+    expect(reducer({}, searchAllBooksSuccess)).toEqual({ searchResult:books })
+  });
+  it('should handle SEARCH_ALL_BOOKS_ERROR', () => {
+    const searchAllBooksError = {
+      type: types.SEARCH_ALL_BOOKS_ERROR,
+      error
+    };
+    expect(reducer({}, searchAllBooksError)).toEqual({ error })
+  });
+  it('should handle DELETE_BOOK_REVIEW_SUCCESS', () => {
+    const deleteBookReviewSuccess = {
+      type: types.DELETE_BOOK_REVIEW_SUCCESS,
+      reviewId: book3.reviews[1].id
+    };
+    expect(reducer({ book: book3 }, deleteBookReviewSuccess)).toEqual({ book:book3AfterReviewDeletion, loadAllReview: true })
+  });
+  it('should handle DELETE_BOOK_REVIEW_ERROR', () => {
+    const deleteBookReviewError = {
+      type: types.DELETE_BOOK_REVIEW_ERROR,
+      error
+    };
+    expect(reducer({}, deleteBookReviewError)).toEqual({ error })
+  });
+  it('should handle LOAD_REVIEW_FOR_EDIT', () => {
+    const loadReviewToEdit = {
+      type: types.LOAD_REVIEW_FOR_EDIT,
+      reviewToEdit: book3.reviews[0]
+    }
+    expect(reducer({}, loadReviewToEdit)).toEqual({ editReview: true, reviewToEdit: book3.reviews[0]})
+  });
+  it('should handle MODIFY_REVIEW_SUCCESS', () => {
+    const modifyBookReviewSuccess = {
+      type: types.MODIFY_REVIEW_SUCCESS,
+      editedReview
+    };
+    expect(reducer({ book: book3 }, modifyBookReviewSuccess)).toEqual({ book:book3AfterReviewUpdate, editReview: false, loadAllReview: true })
+  });
+  it('should handle MODIFY_REVIEW_ERROR', () => {
+    const modifyBookReviewError = {
+      type: types.MODIFY_REVIEW_ERROR,
+      error
+    };
+    expect(reducer({}, modifyBookReviewError)).toEqual({ error })
+  });
+  it('should handle POPULAR_BOOK', () => {
+    const fetchPopularBooksAction = {
+      type: types.POPULAR_BOOK
+    };
+    expect(reducer({}, fetchPopularBooksAction)).toEqual({loadingPopularBooks: true})
+  });
+  it('should handle FETCH_POPULAR_BOOK_SUCCESS', () => {
+    const fetchPopularBooksSuccess = {
+      type: types.FETCH_POPULAR_BOOK_SUCCESS,
+      popularBooks
+    };
+    expect(reducer({}, fetchPopularBooksSuccess))
+    .toEqual({popularBooks, loadingPopularBooks: false})
+  });
+  it('should handle FETCH_POPULAR_BOOK_ERROR', () => {
+    const fetchPopularBooksError = {
+      type: types.FETCH_POPULAR_BOOK_ERROR,
+      error
+    };
+    expect(reducer({}, fetchPopularBooksError))
+    .toEqual({error, loadingPopularBooks: false})
+  });
+  it('should handle TOP_FAVORITED_BOOKS', () => {
+    const fetchTopFavoriteBooksAction = {
+      type: types.TOP_FAVORITED_BOOKS
+    };
+    expect(reducer({}, fetchTopFavoriteBooksAction))
+    .toEqual({loadingTopFavoritedBooks: true})
+  });
+  it('should handle FETCH_TOP_FAVORITED_BOOKS_SUCCESS', () => {
+    const fetchTopFavoriteBooksSuccess = {
+      type: types.FETCH_TOP_FAVORITED_BOOKS_SUCCESS,
+      topFavoriteBooks
+    };
+    expect(reducer({}, fetchTopFavoriteBooksSuccess))
+    .toEqual({topFavoriteBooks, loadingTopFavoritedBooks: false})
+  });
+  it('should handle FETCH_TOP_FAVORITED_BOOKS_ERROR', () => {
+    const fetchTopFavoriteBooksError = {
+      type: types.FETCH_TOP_FAVORITED_BOOKS_ERROR,
+      error
+    };
+    expect(reducer({}, fetchTopFavoriteBooksError))
+    .toEqual({error, loadingTopFavoritedBooks: false})
+  });
+  it('should handle HANDLE_CANCEL_CLICK', () => {
+    const handleCancelClick = {
+      type: types.HANDLE_CANCEL_CLICK
+    }
+    expect(reducer({}, handleCancelClick))
+    .toEqual({ loadAllReview: true, editReview: false })
+  });
   
 })
--- client/tests/reducers/mocks/booksData.js
+++ client/tests/reducers/mocks/booksData.js
@@ -31,7 +31,21 @@ export const book3 = {
   image: 'https://res.cloudinary.com/sardaunan/image/upload/v1518354032/book-default-image_e8nj1p.png',
   quantity: 23,
   updatedAt: '2018-02-20T35:20:01.852Z',
-  createdAt: '2018-02-20T35:20:01.852Z'
+  createdAt: '2018-02-20T35:20:01.852Z',
+  reviews: [
+    {
+      id: 2,
+      content: 'the content',
+      caption: 'the caption then',
+      user: 'anne'
+    },
+    {
+      id: 3,
+      content: 'the test content',
+      caption: 'the test caption',
+      user: 'dami'
+    }
+  ]
 }
 
 export const books = [book1, book2, book3]
@@ -39,3 +53,58 @@ export const error = {}
 export const addfavbook = [book2]
 export const upvotedBook = {}
 export const downvotedBook = {}
+export const book3AfterReviewDeletion = {
+  id: 6,
+  isbn: 365242,
+  title: 'The Secret life of Baba Segi\'s wives',
+  author: 'Lola Shoneyin',
+  description: 'The secret of the lorax features a man selling air',
+  image: 'https://res.cloudinary.com/sardaunan/image/upload/v1518354032/book-default-image_e8nj1p.png',
+  quantity: 23,
+  updatedAt: '2018-02-20T35:20:01.852Z',
+  createdAt: '2018-02-20T35:20:01.852Z',
+  reviews: [
+    {
+      id: 2,
+      content: 'the content',
+      caption: 'the caption then',
+      user: 'anne'
+    }
+  ]
+}
+
+export const reviewToEdit = {}
+export const book3AfterReviewUpdate = {
+  id: 6,
+  isbn: 365242,
+  title: 'The Secret life of Baba Segi\'s wives',
+  author: 'Lola Shoneyin',
+  description: 'The secret of the lorax features a man selling air',
+  image: 'https://res.cloudinary.com/sardaunan/image/upload/v1518354032/book-default-image_e8nj1p.png',
+  quantity: 23,
+  updatedAt: '2018-02-20T35:20:01.852Z',
+  createdAt: '2018-02-20T35:20:01.852Z',
+  reviews: [
+    {
+      id: 2,
+      content: 'the content',
+      caption: 'the caption then',
+      user: 'anne'
+    },
+    {
+      id: 3,
+      content: 'the test content changed',
+      caption: 'the test caption changed',
+      user: 'dami'
+    }
+  ]
+}
+export const editedReview = {
+  id: 3,
+  content: 'the test content changed',
+  caption: 'the test caption changed',
+  user: 'dami'
+}
+
+export const popularBooks = [book3, book1]
+export const topFavoriteBooks = [book2, book1]
